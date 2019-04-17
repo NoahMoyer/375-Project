6ff6fa15e9a37312e79f94d8acfe20ad93f38527
Noah <npmoyer@umich.edu>
2019-04-17 19:26:47 -0400

    saving

--- GraphSharpTutorial01/GraphSharpTutorial01/FileInput.xaml.cs
+++ GraphSharpTutorial01/GraphSharpTutorial01/FileInput.xaml.cs
@@ -26,7 +26,35 @@ namespace RailroadSimulation
     /// </summary>
     public partial class FileInput : Window
     {
-        string[] lines;
+        private string[] lines;
+        private int numberOfHubs, numberOfStations, numberOfEdges, numberOfLocomotives;
+        private Hub[] hubs; //hubs in the railroad
+        private Locomotive[] locomotives; //locomotives in the railroad
+        private Edge[] edges; //edges in the railroad
+        private Station stations; //stations in the railroad
+        struct Hub
+        {
+            private Locomotive[] Locomotives; //locomotives that have this hub as a home
+        }
+        struct Locomotive
+        {
+            private Hub homeHub; //home hub for the locomotive
+            private string type; //either passenger or freight
+        }
+        struct Edge
+        {
+            private float weight; //distance of the edge
+            private Station station1, station2; //stations the edge is connected to
+            private Hub hub; //hub that the edge is connected to. Will not be used for most edges since most edges will not be connected to a hub
+            private float startAvailableTime, endAvailableTime; //the time between that the edge is available
+        }
+        struct Station
+        {
+            private string type; //either passenger or freight
+            private int maxNumberOfTrains; //the maxium number of trains that can be at a station at once
+            private int randomOnRangeLower, randonOnRangeUpper; //range for how many passengers will get on the passenger train. 0 for freight trains
+            private int randonOffRangelower, randomOffRangeUpper; //range for how many passengers will get off the passenger train. 0 for freight trains
+        }
 
         public  FileInput()
         {
@@ -42,9 +70,11 @@ namespace RailroadSimulation
             newRailroadWindow.ShowDialog();
         }
 
+
         private void openFile_Click(object sender, RoutedEventArgs e)
         {
-            int numberOfHubs, numberOfStations, numberOfEdges, numberOfLocomotives;
+            
+
 
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
