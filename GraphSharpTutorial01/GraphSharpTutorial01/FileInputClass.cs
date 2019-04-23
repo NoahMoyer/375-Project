17dccf2633aa8402e79c424a4f63f783fdb4bc9a
Noah <npmoyer@umich.edu>
2019-04-23 16:17:52 -0400

    working on building the railroad

--- GraphSharpTutorial01/GraphSharpTutorial01/FileInputClass.cs
+++ GraphSharpTutorial01/GraphSharpTutorial01/FileInputClass.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using QuickGraph;
 
 namespace GraphSharpTutorial01
 {
@@ -15,13 +16,29 @@ namespace GraphSharpTutorial01
 
         struct Hub
         {
-            private Locomotive[] Locomotives; //locomotives that have this hub as a home
+            private Locomotive[] locomotivesInHub; //locomotives that have this hub as a home
+            private string name; //name of the hub
+
+            public Locomotive[] LocomotivesInHub
+            {
+                get { return locomotivesInHub; }
+                
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
         }
 
         struct Locomotive
         {
             private Hub homeHub; //home hub for the locomotive
             private string type; //either passenger or freight
+            private string name; //name of the locomotive
         }
 
         struct Edge
@@ -38,10 +55,29 @@ namespace GraphSharpTutorial01
             private int maxNumberOfTrains; //the maxium number of trains that can be at a station at once
             private int randomOnRangeLower, randonOnRangeUpper; //range for how many passengers will get on the passenger train. 0 for freight trains
             private int randonOffRangelower, randomOffRangeUpper; //range for how many passengers will get off the passenger train. 0 for freight trains
+            private string name; //name of the station
         }
 
-        public void buildRailroad()
+        public void buildRailroad(int numberOfHubs, int numberOfStations, int numberOfEdges, int numberOfLocomotives)
         {
+            var RailroadGraph = new BidirectionalGraph<object, IEdge<object>>();
+
+            List<Hub> hubs = new List<Hub>(); //list of hubs
+            for (int i = 0; i < numberOfHubs; i++) //adding hubs to the graph as vertices
+            {
+                hubs.
+                RailroadGraph.AddVertex(hubs[i]);
+            }
+
+
+            List<Station> stations = new List<Station>(); //list of stations
+            for (int i = 0; i < numberOfHubs; i++) //adding stations to the graph as verticies
+            {
+                stations.Add("Station " + (i + 1).ToString());
+                RailroadGraph.AddVertex(stations[i]);
+            }
+
+
 
         }
 
