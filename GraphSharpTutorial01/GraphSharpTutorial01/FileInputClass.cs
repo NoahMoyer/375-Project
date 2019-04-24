0f3c2d6c764435c885284a601bed00e894d48752
Noah <npmoyer@umich.edu>
2019-04-23 21:02:12 -0400

    saving

--- GraphSharpTutorial01/GraphSharpTutorial01/FileInputClass.cs
+++ GraphSharpTutorial01/GraphSharpTutorial01/FileInputClass.cs
@@ -14,19 +14,33 @@ namespace GraphSharpTutorial01
         private Edge[] edges; //edges in the railroad
         private Station stations; //stations in the railroad
 
-        struct Hub
+        class Hub
         {
-            private Locomotive[] locomotivesInHub; //locomotives that have this hub as a home
+            List<Locomotive> locomotivesInHub = new List<Locomotive>(); //locomotives that have this hub as a home
             private string name; //name of the hub
 
-            public Locomotive[] LocomotivesInHub
+            public List<Locomotive> LocomotivesInHub //get method for locommotivesInHub
             {
                 get { return locomotivesInHub; }
                 
 
             }
 
-            public string Name
+            public void addLocomotiveToHub(Locomotive LocomotiveToAddToHub) //add a locomotive in a hub
+            {
+                locomotivesInHub.Add(LocomotiveToAddToHub);
+            }
+
+            public void removeLocomotiveFromHub(Locomotive LocomotiveToRemoveFromHub) //remove a locomotive in a hub
+            {
+                if (locomotivesInHub.Contains(LocomotiveToRemoveFromHub))
+                {
+                    locomotivesInHub.Remove(LocomotiveToRemoveFromHub);
+                }
+            }
+
+
+            public string Name //get and set for name
             {
                 get { return name; }
                 set { name = value; }
@@ -34,28 +48,34 @@ namespace GraphSharpTutorial01
 
         }
 
-        struct Locomotive
+        class Locomotive
         {
             private Hub homeHub; //home hub for the locomotive
             private string type; //either passenger or freight
             private string name; //name of the locomotive
+
+
         }
 
-        struct Edge
+        class Edge
         {
             private float weight; //distance of the edge
             private Station station1, station2; //stations the edge is connected to
             private Hub hub; //hub that the edge is connected to. Will not be used for most edges since most edges will not be connected to a hub
             private float startAvailableTime, endAvailableTime; //the time between that the edge is available
+
+
         }
 
-        struct Station
+        class Station
         {
             private string type; //either passenger or freight
             private int maxNumberOfTrains; //the maxium number of trains that can be at a station at once
             private int randomOnRangeLower, randonOnRangeUpper; //range for how many passengers will get on the passenger train. 0 for freight trains
             private int randonOffRangelower, randomOffRangeUpper; //range for how many passengers will get off the passenger train. 0 for freight trains
             private string name; //name of the station
+
+
         }
 
         public void buildRailroad(int numberOfHubs, int numberOfStations, int numberOfEdges, int numberOfLocomotives)
